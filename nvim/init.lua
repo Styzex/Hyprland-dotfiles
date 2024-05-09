18b87a913c4c67d87f1e35b0d0815137628aefb3
Styzex <120598476+Styzex@users.noreply.github.com>
2024-05-09 21:27:05 +0200

    Updated dotfiles
    also added  my nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+
+require("vim-options")
+require("lazy").setup("plugins")
+vim.keymap.set('n', '<leader>q', ':qa', {})
+vim.keymap.set('n', '<leader>w', ':wqa', {})
+
+require("gruvbox").setup()
+vim.cmd("colorscheme gruvbox")
